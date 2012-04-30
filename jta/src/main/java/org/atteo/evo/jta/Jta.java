af77cd4c3c57aded3772c4e1f73a2216c68c5b96
Sławek Piotrowski <sentinel@atteo.com>
2012-04-30 12:41:42 +0200

    Filter which wraps request inside transaction
    Change-Id: I24d5900ade04c24926cfe09a43910e6515144cb9
    Reviewed-on: https://pirx.atteo.com/gerrit/92
    Tested-by: Mr. Jenkins
    Reviewed-by: Sławek Piotrowski <sentinel@atteo.com>

--- jta/src/main/java/org/atteo/evo/jta/Jta.java
+++ jta/src/main/java/org/atteo/evo/jta/Jta.java
@@ -1,12 +1,15 @@
 package org.atteo.evo.jta;
 
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import org.atteo.evo.config.XmlDefaultValue;
 import org.atteo.evo.services.TopLevelService;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Module;
 import com.google.inject.matcher.Matchers;
+import com.google.inject.servlet.ServletModule;
 
 /**
  * Generic JTA support service.
@@ -21,15 +24,25 @@ import com.google.inject.matcher.Matchers;
  */
 @XmlRootElement(name = "jta")
 public class Jta extends TopLevelService {
+	/**
+	 * Register {@link JtaFilter} which wraps web requests handling inside JTA transaction.
+	 */
+	@XmlElement
+	private boolean registerWebFilter = false;
+
 	@Override
 	public Module configure() {
-		return new AbstractModule() {
+		return new ServletModule() {
 			@Override
-			protected void configure() {
+			protected void configureServlets() {
 				requestStaticInjection(Transaction.class);
 				TransactionalInterceptor interceptor = new TransactionalInterceptor();
 				bindInterceptor(Matchers.any(), Matchers.annotatedWith(Transactional.class), interceptor);
 				bindInterceptor(Matchers.annotatedWith(Transactional.class), Matchers.any(), interceptor);
+
+				if (registerWebFilter) {
+					filter("/*").through(JtaFilter.class);
+				}
 			}
 		};
 	}
