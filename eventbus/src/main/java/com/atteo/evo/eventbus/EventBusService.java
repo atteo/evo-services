93f6f5311edbb0d2a847fdf2080787629b69ae4a
Tomek Piotrowski <tomek.piotrowski@atteo.com>
2012-07-27 22:55:34 +0200

    Guava EventBus
    Change-Id: Ib60f66af1a88fd0d5cc86514825bf84b4b53fee0
    Reviewed-on: https://pirx.atteo.com/gerrit/111
    Tested-by: Mr. Jenkins
    Reviewed-by: Sławek Piotrowski <sentinel@atteo.com>

---
+++ eventbus/src/main/java/com/atteo/evo/eventbus/EventBusService.java
@@ -0,0 +1,35 @@
+package com.atteo.evo.eventbus;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.atteo.evo.services.TopLevelService;
+
+import com.google.common.eventbus.EventBus;
+import com.google.inject.Binder;
+import com.google.inject.Module;
+import com.google.inject.Singleton;
+/**
+ * Simple server-side event bus.
+ */
+@XmlRootElement(name = "eventbus")
+public class EventBusService extends TopLevelService {
+	@Override
+	public Module configure() {
+		return new Module() {
+
+			@Override
+			public void configure(Binder binder) {
+					binder.bind(EventBus.class).in(Singleton.class);
+			}
+		};
+	}
+
+	@Override
+	public void start() {
+	}
+
+	@Override
+	public void stop() {
+	}
+
+}
