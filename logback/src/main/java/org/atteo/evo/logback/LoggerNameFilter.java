1e687472c647f2b5d84589c6dbf8ccfff57e795b
Tomek Piotrowski <tomek.piotrowski@atteo.com>
2012-12-08 17:47:56 +0100

    Filtering logs by package name
    Change-Id: If5aaec4d15bf048409945af44e9d2a66221ec61d
    Reviewed-on: https://pirx.atteo.com/gerrit/137
    Tested-by: Mr. Jenkins
    Reviewed-by: Sławek Piotrowski <sentinel@atteo.com>

---
+++ logback/src/main/java/org/atteo/evo/logback/LoggerNameFilter.java
@@ -0,0 +1,35 @@
+package org.atteo.evo.logback;
+
+import ch.qos.logback.classic.spi.LoggingEvent;
+import ch.qos.logback.core.filter.AbstractMatcherFilter;
+import ch.qos.logback.core.spi.FilterReply;
+
+public class LoggerNameFilter extends AbstractMatcherFilter<LoggingEvent> {
+
+	private String loggerName;
+
+	@Override
+	public FilterReply decide(LoggingEvent event) {
+		if (event.getLoggerName().startsWith(loggerName)) {
+			return onMatch;
+		} else {
+			return onMismatch;
+		}
+	}
+
+	public void setLoggerName(String loggerName) {
+		this.loggerName = loggerName;
+	}
+
+	public String getLoggerName() {
+		return loggerName;
+	}
+
+	@Override
+	public void start() {
+		if (loggerName != null) {
+			super.start();
+		}
+	}
+
+}
